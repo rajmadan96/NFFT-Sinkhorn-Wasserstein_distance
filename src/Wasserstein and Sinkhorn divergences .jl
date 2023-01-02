40ec2413560762b5a984ac4fdd7aa7bbd27ca273
Rajmadan <62553056+rajmadan96@users.noreply.github.com>
2023-01-02 11:44:42 +0100

    Stopping criteria and log Sinkhorn (stablized) updated

--- src/Wasserstein and Sinkhorn divergences .jl
+++ src/Wasserstein and Sinkhorn divergences .jl
@@ -1,5 +1,6 @@
 using LinearAlgebra
 using JuMP, Tulip			# Coin-or linear programming
+using LogExpFunctions
 include("ontoSimplex.jl")
 
 
@@ -32,23 +33,62 @@ function Wasserstein(p1::Vector{Float64}, p2::Vector{Float64}, distMatrix::Array
     return (distance= (objective_value(model))^(1/ rWasserstein), π= reshape(value.(x), (n1, n2)))
 end
 
-function Sinkhorn(p1::Vector{Float64}, p2::Vector{Float64}, distMatrix::Array{Float64,2}; rWasserstein::Float64= 1., λ::Float64= 1.)
+function Sinkhorn(p1::Vector{Float64}, p2::Vector{Float64}, distMatrix::Array{Float64,2}; rWasserstein::Float64= 1., λ::Float64= 1.,tol=1e-5,max_iter=1000,verbose=false)
 	ontoSimplex!(p1); βr= Vector{Float64}(undef, length(p1))
 	ontoSimplex!(p2); γc= ones(size(p2))		# guess a starting value
-	count= 0; tmp= Vector{Float64}(undef, length(p2)); tmp0= -Inf; tmp1= -floatmax()
+	count= 0; 
 	distMatrix.^= rWasserstein; K= exp.(-λ * distMatrix)
-	while tmp0 < tmp1 && count < 1000	# Sinkhorn iteration
+	while true 	# Sinkhorn iteration
 		γc= γc./ (p2'*γc)		# rescale
 		βr= p1./ (K* γc)		# vector operation
 		tmp= K'* βr
 		γc= p2./ tmp			# vector operation
-		count += 1; tmp0, tmp1= tmp1, 1+p1'*replace!(log.(βr),-Inf=>0) + p2'*replace!(log.(γc),-Inf=>0)- tmp'* γc
+		count += 1;
+		if count % 10 == 0
+			err_βr=
+				 norm(real((K* γc).* βr)- real(p1),2) #/ max(maximum(abs.(real(βr))), maximum(abs.(real(βr_old))), 1)
+			 err_γc=
+				 norm(real(tmp.*γc)- real(p2),2) #/ max(maximum(abs.(real(γc))), maximum(abs.(real(γc_old))), 1)
+			 if verbose
+				 println("Iteration $count, err = ",  (err_βr+ err_γc))
+				 end
+			 if ((err_βr+ err_γc) < tol) || count > max_iter
+				 break
+			 end
+		 end
 	end
-	return (distSinkhorn= tmp0/ λ,
-			distSinkhornUB= (tmp0-p1'*replace!(log.(p1),-Inf=>0)-p2'*replace!(log.(p2),-Inf=>0))/ λ,
+	tmp=1+p1'*replace!(log.(βr),-Inf=>0) + p2'*replace!(log.(γc),-Inf=>0)- tmp'* γc
+	return (distSinkhorn= tmp/ λ,distSinkhornUB= (tmp-p1'*replace!(log.(p1),-Inf=>0)-p2'*replace!(log.(p2),-Inf=>0))/ λ,
 			count= count)
 end
 
+	
+function logSinkhorn_stablized(p1::Vector{Float64}, p2::Vector{Float64}, distMatrix::Array{Float64,2}; rWasserstein::Float64= 1., λ::Float64= 1.,tol=1e-5,max_iter=1000,verbose=false)
+	ontoSimplex!(p1); βr=zeros(size(p1))
+	ontoSimplex!(p2); γc= zeros(size(p2))		# guess a starting value
+	count= 0; 
+	distMatrix.^= rWasserstein; K= exp.(-λ * distMatrix)
+	while true	# Sinkhorn iteration
+		βr= (log.(p1.+ 1e-20).- vec(logsumexp((λ *(- distMatrix.+γc'));dims=2)))./λ		# vector operation
+
+		γc= (log.(p2.+ 1e-20).- vec((logsumexp((λ *(- distMatrix.+βr ));dims=1))))./λ		# vector operation
+		count += 1;
+		if count % 10 == 0
+			err_βr=
+				norm(real((K* exp.(λ*γc)).* exp.(λ*βr))- real(p1),2) 
+			err_γc=
+				norm(real((K'* exp.(λ*βr)).*exp.(λ*γc))- real(p2),2) 
+			if verbose
+				println("Iteration $count, err = ",  (err_βr+ err_γc))
+			end
+			if ((err_βr+ err_γc) < tol) || count > max_iter
+				break
+			end
+		end
+	end
+	f=((βr));g=((γc));
+	return (f=βr,g=γc,count=count)
+end
 
 
 
